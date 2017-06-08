ef72f2305b26dbf64e849de08575531a3b2ab5a6
Dan <dangreen.tlv@gmail.com>
2017-06-08 08:22:13 -0400

    minor refactoring in configureStore

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,15 +1,6 @@
 import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import reducer from './reducers';
-/*global process */
-const middleware = applyMiddleware(thunk);
-export default () =>
-  process.env.NODE_ENV === 'test'
-    ? createStore(reducer, middleware)
-    : createStore(
-        reducer,
-        compose(
-          middleware,
-          window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-        )
-      );
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+export default () => createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
